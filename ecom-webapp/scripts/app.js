18e249e02baa86ee4c4aef4ba0136eca144423b0
Nic Ferrier <nferrier@ferrier.me.uk>
2014-08-06 23:34:47 +0100

    move the video click event out to separate func

--- ecom-webapp/scripts/app.js
+++ ecom-webapp/scripts/app.js
@@ -4,34 +4,49 @@ var userMedia = require("./userMedia.js");
 var Hammer = require("hammerjs");
 var formSaver = require ("./formSaver.js");
 
+function cameraSnap (videoObject) {
+  var canvasElement = document.createElement("canvas");
+  canvasElement.setAttribute("class", "hidden");
+  document.body.appendChild(canvasElement);;
+  var width = $(videoObject).width(), height = $(videoObject).height();
+  canvasElement.style.width = width + "px";
+  canvasElement.style.height = height + "px";
+  canvasElement.width = width;
+  canvasElement.height = height;
+
+  var graphicContext = canvasElement.getContext("2d");
+  graphicContext.clearRect(0, 0, width, height);
+  graphicContext.drawImage(videoObject, 0, 0, width, height);
+  userMedia.stopCapture(document.getElementById("vid"));
+
+  var imgData = canvasElement.toDataURL();
+  $("#capture").append(
+    util.format(
+      "<img src=\"%s\" title=\"click again to take another\"></img>", 
+      imgData
+    )
+  );
+  $(videoObject).addClass("hidden");
+
+  var img = $("#capture img");
+  img.on("dragstart", function () { return false; });
+  var mc = Hammer(img[0]);
+  mc.on(
+    "swipeleft", 
+    function () { 
+      alert("swiped!"); 
+    }
+  );
+}
+
 function cameraOn () {
   $("#capture").html("<video id='vid' autoplay=\"yes\"></video>");
   var videoObject = document.getElementById("vid");
   userMedia.doGetUserMedia(
     videoObject,
     function () {
-      var mc = Hammer(videoObject);
-      mc.on("swipeleft swiperight", function () { console.log("swiped!"); });
       $(videoObject).click(function () {
-        var canvasElement = document.createElement("canvas");
-        canvasElement.setAttribute("class", "hidden");
-        document.body.appendChild(canvasElement);;
-        var width = $(videoObject).width(), height = $(videoObject).height();
-        canvasElement.style.width = width + "px";
-        canvasElement.style.height = height + "px";
-        canvasElement.width = width;
-        canvasElement.height = height;
-        var graphicContext = canvasElement.getContext("2d");
-        graphicContext.clearRect(0, 0, width, height);
-        graphicContext.drawImage(videoObject, 0, 0, width, height);
-        userMedia.stopCapture(document.getElementById("vid"));
-        $("#capture").append(
-          util.format(
-            "<img src=\"%s\" title=\"click again to take another\"></img>", 
-            canvasElement.toDataURL()
-          )
-        );
-        $(videoObject).addClass("hidden");
+        cameraSnap(videoObject);
       });
     }
   );
@@ -41,7 +56,5 @@ cameraOn();
 
 // not sure about "change" - it doesn't tell us very quickly
 formSaver.attach(document.forms["inventory"]);
-
-
 
 // app.js ends here
